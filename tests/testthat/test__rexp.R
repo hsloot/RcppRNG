c6d9a726d1a3cf7ccdb4e77b541e7936810389ac
Henrik Sloot <Henrik.Sloot@gmail.com>
2020-05-10 15:46:03 +0200

    add RNGScope tests and reorganise tests

--- tests/testthat/test__rexp.R
+++ tests/testthat/test__rexp.R
@@ -1,11 +1,52 @@
 context("rexp")
 
+setup({
+  # make sure that default RNGkind is used
+  RNGkind(kind="default", normal.kind="default", sample.kind="default")
+  if(requireNamespace("dqrng", quietly=TRUE)) {
+    dqRNGkind("default")
+    dqrng::dqRNGkind("default")
+  }
+})
+
 use_seed <- 1623L
-dqrng_flag <- FALSE
+
+# sampling parameters
+n <- 1e4
+args <- list("rate" = 0.5)
+
+test_that("rexp with Rcpp::RNGScope is equivalent to stats::rexp", {
+  expect_equal_sampling_result(Rcpp_rexp_RNGScope, stats::rexp, arguments = args,
+    n=n, use_seed=use_seed)
+
+  Rcpp_rexp_RNGScope_ <- function(n, rate) { # nolint
+    sapply(1:n, function(x) Rcpp_rexp_RNGScope(1, rate))
+  }
+  rexp_ <- function(n, rate) {
+    sapply(1:n, function(x) stats::rexp(1, rate))
+  }
+
+  expect_equal_sampling_result(Rcpp_rexp_RNGScope_, rexp_, arguments = args,
+    n=n, use_seed=use_seed)
+
+  x_ <- function(n, rate) {
+    c(
+      sapply(1:n, function(x) Rcpp_rexp_RNGScope(1, rate)),
+      sapply(1:n, function(x) rexp(1, rate))
+    )
+  }
+  y_ <- function(n, rate) {
+    c(
+      sapply(1:n, function(x) rexp(1, rate)),
+      sapply(1:n, function(x) Rcpp_rexp_RNGScope(1, rate))
+    )
+  }
+  expect_equal_sampling_result(x_, y_, arguments = args, n=n,
+    use_seed=use_seed)
+})
+
 
 test_that("rexp with RcppRNG is equivalent to stats::rexp", {
-  args <- list("rate" = 0.5)
-  n <- 1e5
   expect_equal_sampling_result(Rcpp_rexp_RCPPRNG, stats::rexp, arguments = args,
     n=n, use_seed=use_seed)
 
@@ -38,10 +79,6 @@ test_that("rexp with RcppRNG is equivalent to stats::rexp", {
 
 test_that("rexp with DQRNG is equivalent to dqrng::rexp", {
   skip_if_not_installed("dqrng")
-  dqRNGkind("default")
-  dqrng::dqRNGkind("default")
-  args <- list("rate" = 0.5)
-  n <- 1e5
   expect_equal_sampling_result(Rcpp_rexp_DQRNG, dqrng::dqrexp,
     arguments = args, n=n, use_seed=use_seed, dqrng_flag=TRUE)
 
